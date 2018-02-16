e268baef137bb560ee1563b44f995017d0399dff
ssganzerla <simone.sganzerla@capgemini.com>
2018-02-16 16:58:18 +0100

    Modificata gestione colori, tempi e livelli, creata funzione getColor

--- sketch_Scala/sketch_Scala.ino
+++ sketch_Scala/sketch_Scala.ino
@@ -7,48 +7,40 @@
 #include "FastLED.h"
 #include <SoftwareSerial.h>
 
-#define NUM_LEDS 40   //n. led nella striscia CRI = 100
-#define DATA_PIN 9    //uscita per collegamento striscia led
-#define PIR_BASSO 2   //PIR Piano Terra
-#define PIR_ALTO 4    //PIR Primo Piano
-#define PULS_PIN 8    //pulsante per accensione manuale
+#define NUM_LEDS 40  //n. led nella striscia CRI = 100
+#define DATA_PIN 9   //uscita per collegamento striscia led
+#define PIR_BASSO 2  //PIR Piano Terra
+#define PIR_ALTO 4   //PIR Primo Piano
+#define PULS_PIN 8   //pulsante per accensione manuale
 #define FOTO_PIN A0  //Fotoresistenza
-#define BLACK 0x000000
-#define WHITE 0xFFFFFF
-#define RED 0xFF0000
-#define GREEN 0x00FF00
-#define BLUE 0x0000FF
-#define YELLOW 0xFFFF00
-#define PURPLE 0xFF00FF
-#define AZURE 0x00FFFF
+
+//Struct per il colore e array di colori
+typedef struct {
+  String nome;
+  uint32_t valore;
+  } colore;
+colore colori[10];
 
 //Oggetto COM Bluettoth e Variabile buffer
 SoftwareSerial bluetooth(6, 7); //BLUETOOTH: PIN TXD 6, PIN RXD 7
 String BLUETOOTH_BUFFER = "";
 
-//Variabili per funzionamento scala
+//Variabili per funzionamento scala e pulsante
 boolean sign_PIR_BASSO=false;
 boolean sign_PIR_ALTO=false;
+boolean sign_PULS=false;
+boolean luce = false;
 boolean mov_salita=false;
 boolean mov_discesa=false;
-uint16_t tempo_salita_ON = 100;
-uint16_t tempo_salita_OFF = 100;
-uint16_t tempo_discesa_ON = 100;
-uint16_t tempo_discesa_OFF = 100;
-uint32_t colore_salita_ON = WHITE;
-uint32_t colore_salita_OFF = BLACK;
-uint32_t colore_discesa_ON = WHITE;
-uint32_t colore_discesa_OFF = BLACK;
+uint16_t tempi[6] = {100, 100, 100, 100, 0, 0}; //salita ON, salita OFF, discesa ON, discesa OFF, pulsante ON, pulsante OFF
+uint32_t colori_movimento[6] = {0xFFFFFF, 0x000000, 0xFFFFFF, 0x000000, 0xFFFFFF, 0x000000};
+uint8_t livelli[6] = {140, 140, 140, 140, 140, 140};
 
 //Variabili per gestione Fotoresistenza
 boolean usaFOTO = true;
 boolean presenzaLUCE =true;
 uint16_t sign_LUCE=0;
 uint16_t sogliaBUIO=380;
-
-//Varaibili per gestione Pulsante Manuale
-boolean sign_PULS=false;
-boolean luce = false;
 
 // Definisco n. led nella striscia
 CRGB leds[NUM_LEDS];
@@ -60,6 +52,14 @@ void setup()
   Serial.println ("SCALA START");
   FastLED.addLeds<WS2811, DATA_PIN, BRG>(leds, NUM_LEDS);   //Inizializzo Array di LED
   bluetooth.begin(9600);  //Inizializzo COM Bluetooth
+  colori[0] = {"BLK", 0x000000}; //BLACK
+  colori[1] = {"WHT", 0xFFFFFF}; //WHITE
+  colori[2] = {"RED", 0xFF0000}; //RED
+  colori[3] = {"GRN", 0x00FF00}; //GREEN
+  colori[4] = {"BLU", 0x0000FF}; //BLUE
+  colori[5] = {"YEL", 0xFFFF00}; //YELLOW
+  colori[6] = {"PUR", 0xFF00FF}; //PURPLE
+  colori[7] = {"AZU", 0x00FFFF}; //AZURE
 }
 
 void cambia_led(uint8_t posizione, uint16_t livello, uint32_t color)
@@ -106,14 +106,14 @@ void letturaPir()
       //STO SALENDO-ACCENDO LUCI IN SALITA
       if (mov_salita==false and mov_discesa==false)
       {    
-        cambia_striscia(0, NUM_LEDS, tempo_salita_ON, 100, colore_salita_ON);
+        cambia_striscia(0, NUM_LEDS, tempi[0], livelli[0], colori_movimento[0]);
         mov_salita=true;
         luce = true;
       }
       //SONO SCESO-SPENGO LUCI IN DISCESA
       if (mov_salita==false and mov_discesa==true)
       {  
-        cambia_striscia(NUM_LEDS, 0, tempo_discesa_OFF, 100, colore_discesa_OFF);
+        cambia_striscia(NUM_LEDS, 0, tempi[3], livelli[3], colori_movimento[3]);
         mov_discesa=false;
         luce = false;
       }
@@ -124,14 +124,14 @@ void letturaPir()
       // STO SCENDENDO-ACCENDO LUCI IN DISCESA
       if (mov_salita==false and mov_discesa==false )
       {
-        cambia_striscia(NUM_LEDS, 0, tempo_discesa_ON, 100, colore_discesa_ON);
+        cambia_striscia(NUM_LEDS, 0, tempi[2], livelli[2], colori_movimento[2]);
         mov_discesa=true;
         luce = true;
       }     
       // SONO SALITO-SPENGO LUCI IN SALITA
       if (mov_salita==true and mov_discesa==false )
       {
-        cambia_striscia(0, NUM_LEDS, tempo_salita_OFF, 100, colore_salita_OFF);
+        cambia_striscia(0, NUM_LEDS, tempi[1], livelli[1], colori_movimento[1]);
         mov_salita=false;
         luce = false;
       }  
@@ -162,12 +162,12 @@ void read_BUTTON()
   if (sign_PULS==true and luce==true)
   {
     luce=false;
-    cambia_striscia(0, NUM_LEDS, 0, 100, BLACK);
+    cambia_striscia(0, NUM_LEDS, tempi[5], livelli[5], colori_movimento[5]);
   }
   else if (sign_PULS==true and luce==false)
   {
     luce=true;
-    cambia_striscia(0, NUM_LEDS, 0, 100, WHITE);
+    cambia_striscia(0, NUM_LEDS, tempi[4], livelli[4], colori_movimento[4]);
   }
 }
 
@@ -217,7 +217,9 @@ void BLUETOOTH_COMMAND()
   if (BLUETOOTH_BUFFER.substring(0, 4) == "set ")
     SET_TEMPO_COLORE(BLUETOOTH_BUFFER.substring(4, BLUETOOTH_BUFFER.length()));
   if (BLUETOOTH_BUFFER.substring(0, 4) == "foto")
-    SET_FOTO(BLUETOOTH_BUFFER.substring(4, BLUETOOTH_BUFFER.length())); 
+    SET_FOTO(BLUETOOTH_BUFFER.substring(4, BLUETOOTH_BUFFER.length()));
+  if (BLUETOOTH_BUFFER.substring(0, 4) == "btn ")
+    colori_movimento[4]=getColor(BLUETOOTH_BUFFER.substring(4, BLUETOOTH_BUFFER.length()));
   BLUETOOTH_BUFFER = "";
 }
 
@@ -228,18 +230,18 @@ void SET_TEMPO_COLORE(String s)
   String tempo = s.substring(s.indexOf("TEMPO:")+6,s.length());
   if (direzione == "SU" && colore != "BLK")
   {
-    tempo_salita_ON = tempo.toInt();
-    colore_salita_ON = WHITE;
+    tempi[0] = tempo.toInt();
+    colori_movimento[0] = getColor(colore);
   }
   if (direzione == "GIU" && colore != "BLK")
   {
-    tempo_discesa_ON = tempo.toInt();
-    colore_discesa_ON = WHITE;
+    tempi[2] = tempo.toInt();
+    colori_movimento[2] = getColor(colore);
   }
   if (direzione == "SU" && colore == "BLK")
-    tempo_salita_OFF = tempo.toInt();
+    tempi[1] = tempo.toInt();
   if (direzione == "GIU" && colore == "BLK")
-    tempo_discesa_OFF = tempo.toInt();
+    tempi[3] = tempo.toInt();
 }
 
 void SET_FOTO(String s)
@@ -248,4 +250,19 @@ void SET_FOTO(String s)
     usaFOTO = true;
   if (s == "OFF")
     usaFOTO = false;
+}
+
+uint32_t getColor(String nome)
+{
+  uint32_t valore=0xFFFFFF;
+  boolean trovato = false;
+  for(uint8_t i=0;i<8 and trovato == false;i++)
+  {
+    if (colori[i].nome == nome)
+    {
+      valore = colori[i].valore;
+      trovato = true;
+    }
+  }
+  return valore;
 }
